2d2e1a41b3b58c11be74616b3bb562f442b7aebe
Sveta5859 <lana.1chebotareva@mail.ru>
2025-01-08 16:50:57 +0300

    all third task

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.cgvsu {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires vecmath;
+    requires java.desktop;
+
+
+
+    opens com.cgvsu to javafx.fxml;
+    exports com.cgvsu;
+}
